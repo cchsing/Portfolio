0e92e1b44f644159f6e4b9024f579710cb84d392
cchsing <jcch950607@gmail.com>
2021-09-15 16:36:34 +0800

    new files and folders

---
+++ Version0/scripts/script.js
@@ -0,0 +1,27 @@
+// const navContent = `
+// <nav class="menu">
+//   <div class="menu-top">
+//     <ul class="menu-items">
+//       <li><a href="./index.html">Home</a></li>
+//       <li><a href="#projects">Projects</a></li>
+//       <li><a href="#work-experience">Work Exp.</a></li>
+//       <li><a href="#education">Education</a></li>
+//       <li><a href="#contact">Contact</a></li>
+//     </ul>
+//   </div>
+//   <div class="menu-bottom">
+//     <ul class="menu-items">
+//       <li><a href="projects/completed.html">Completed</a></li>
+//       <li><a href="projects/ongoing.html">Ongoing</a></li>
+//       <li><a href="projects/newIdeas.html">New Ideas</a></li>
+//     </ul>
+//   </div>
+// </nav>
+// `;
+// const mainNav = document.createElement("nav")
+// mainNav.classList.add("main-navigation");
+// const navList = document.createElement("ul")
+// navList.innerHTML = navContent
+// mainNav.append(navList)
+
+// document.querySelector(".siteheader").append(mainNav)
