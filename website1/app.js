808fb89b8fd57c0ce5ef921ca14d03a1d9b39152
cchsing <jcch950607@gmail.com>
2022-06-05 19:27:46 +0800

    updateLightMode

--- website1/app.js
+++ website1/app.js
@@ -26,6 +26,12 @@ function PageTransition(){
         }
         // console.log(Event.target)
     })
+    // Toggle theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click', ()=>{
+        let element = document.body;
+        element.classList.toggle('light-mode')
+    })
 }
 
 PageTransition();
